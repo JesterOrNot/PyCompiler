49116b84e0f6e7d76a72e00548f314fe06197d0e
Sean Hellum <seanhellum45@gmail.com>
2019-12-30 04:06:01 +0000

    add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        let _file_name = args[1].clone();
+    } else {
+        println!("\x1b[0;31mError: \x1b[0mPlease Specify a file\n\x1b[1;32mUsage:\x1b[0m py_compiler [filename]");
+    }
+}
+
+#[allow(dead_code, unused_variables)]
+fn lexer(line: &str) {
+    //
+}
+
+pub enum Tokens {
+    Keyword,
+    Comment,
+    Operator,
+    Identifier,
+    OpenDelimiter,
+    CloseDelimiter,
 }
